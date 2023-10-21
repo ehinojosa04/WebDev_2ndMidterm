aace9f5ec5f1622cc9c09b0ee2df54e625431af6
EmilianoHinojosa <myemail>
2023-10-20 20:03:37 -0600

    Pokemon_view

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require ("body-parser");
 var https = require('https');
+const { render } = require('ejs');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,9 +10,15 @@ app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 
-app.get('/home',(req,res)=>{
-    res.render('pokemon_view');
-})
+app.get('/pokemon_view/:id', async (req, res) => {
+    const id = req.params.id;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    let data;
+    const response = await fetch(url);
+    data = await response.json();
+    res.render('pokemon_view', { pokemon: data });
+});
+
 
 app.listen(5000,() =>{
     console.log("Listening to port 5000...");
